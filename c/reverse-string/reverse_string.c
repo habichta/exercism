0c359604c089a23bc6afb2c18c67e2e34a85621c
habichta <arthur.habicht@sedimentum.com>
2023-09-24 22:55:23 +0200

    reversed string

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,1 +1,19 @@
 #include "reverse_string.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+char *reverse(const char *value) {
+  size_t len = strlen(value);
+  char *reversed = (char *)malloc((len + 1) * sizeof(char));
+
+  size_t j = 0;
+
+  for (int i = len - 1; i >= 0; i--) {
+    reversed[j] = value[i];
+    j++;
+  }
+
+  reversed[len] = '\0';
+
+  return reversed;
+}
