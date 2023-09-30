614c6445d042b5cbc029f438dc0740a15efbef38
habichta <arthur.habicht@sedimentum.com>
2023-09-30 18:57:28 +0200

    simplified saddle points

--- c/saddle-points/saddle_points.c
+++ c/saddle-points/saddle_points.c
@@ -2,35 +2,67 @@
 #include "stdlib.h"
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
 
 saddle_points_t *saddle_points(size_t x, size_t y, uint8_t matrix[x][y]) {
 
-  size_t rows[x][2];
-  size_t cols[y][2];
+  saddle_points_t *saddle_points =
+      malloc(sizeof(size_t) + sizeof(saddle_point_t));
 
-  for (int row = 0; row < x; row++) {
-    for (int col = 0; col < y; col++) {
+  saddle_points->count = 0;
 
-      // go through matrix and save index of largest point / smallest point row
-      // and columnwise check if both row and col have the same index somewhere
-      // -> match
-    }
+  int rows_max[x];
+  int cols_min[y];
+  int rows_max_ind[x];
+  int cols_min_ind[y];
+
+  for (size_t i = 0; i < x; i++) {
+    rows_max[i] = -1;
+    rows_max_ind[i] = 0;
   }
 
-  for (int row = 0; row < x; row++) {
+  for (size_t i = 0; i < y; i++) {
+    cols_min[i] = 1024;
+    cols_min_ind[i] = 0;
+  }
 
-    for (int col = 0; col < y; col++) {
+  for (size_t row = 0; row < x; row++) {
 
-      uint8_t point = matrix[row][col];
-      uint8_t point_west = matrix[row][col + 1];
-      uint8_t point_east = matrix[row][col - 1];
-      uint8_t point_north = matrix[row + 1][col];
-      uint8_t point_south = matrix[row - 1][col];
+    for (size_t col = 0; col < y; col++) {
+      int current_value = matrix[row][col];
 
-      if (point) {
+      if (rows_max[row] <= current_value) {
+        rows_max[row] = current_value;
+        rows_max_ind[row] = col;
+      }
+      if (cols_min[col] >= current_value) {
+        cols_min[col] = current_value;
+        cols_min_ind[col] = row;
       }
     }
   }
+
+  for (size_t i = 0; i < x; i++) {
+
+    int max_col_ind = rows_max_ind[i];
+
+    if (max_col_ind != -1) {
+      int max_row_ind = cols_min_ind[max_col_ind];
+
+      if (max_row_ind == (int)i) {
+        saddle_points->count++;
+
+        saddle_points =
+            realloc(saddle_points, sizeof(size_t) + saddle_points->count *
+                                                        sizeof(saddle_point_t));
+        saddle_point_t sp = {.row = i + 1, .column = max_col_ind + 1};
+
+        saddle_points->points[saddle_points->count - 1] = sp;
+      }
+    }
+  }
+  return saddle_points;
 }
 
 void free_saddle_points(saddle_points_t *saddle_points) { free(saddle_points); }
